8e80c64aca15a9f550c99b02cf747fc3b5715a30
Andre <andre@eidemann.de>
2024-03-19 22:10:08 +0000

    Fix user state refreshing and error handling

--- custom_components/rehau_nea_smart_2/rehau_mqtt_client/handlers/auth.py
+++ custom_components/rehau_nea_smart_2/rehau_mqtt_client/handlers/auth.py
@@ -12,17 +12,21 @@ from ..utils import generate_auth_url
 from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
+CLIENT_ID = "3f5d915d-a06f-42b9-89cc-2e5d63aa96f1"
+CLIENT_SECRET = "10edca85-0623-48ad-bbbe-76b5e4ec89a9"
+AUTH_URL_ROLES = "email%20roles%20profile%20offline_access"
+AUTH_URL_REDIRECT = "http://localhost:3000/%23!/auth-code"
+AUTH_URL_ORIGIN = "https://accounts.rehau.com"
 
 
 async def auth(email, password, check_credentials=False):
     """Authenticate with Rehau NEA Smart 2."""
-
     challenge = secrets.token_urlsafe(16)
     url = await generate_auth_url(
-        "3f5d915d-a06f-42b9-89cc-2e5d63aa96f1",
-        "email%20roles%20profile%20offline_access",
-        "http://localhost:3000/%23!/auth-code",
-        "https://accounts.rehau.com",
+        CLIENT_ID,
+        AUTH_URL_ROLES,
+        AUTH_URL_REDIRECT,
+        AUTH_URL_ORIGIN,
         challenge
     )
     async with httpx.AsyncClient() as client:
@@ -40,14 +44,13 @@ async def auth(email, password, check_credentials=False):
             "requestId": request_id,
             "rememberMe": "true",
         }
-
-        response = await client.post("https://accounts.rehau.com/login-srv/login", timeout=30, data=data, headers={
+        headers = {
             "Content-Type": "application/x-www-form-urlencoded",
-            "Origin": "https://accounts.rehau.com",
-            "Referer": "https://accounts.rehau.com/rehau-ui/login?requestId={}&view_type=login".format(request_id),
+            "Origin": AUTH_URL_ORIGIN,
+            "Referer": AUTH_URL_ORIGIN + "/rehau-ui/login?requestId={}&view_type=login".format(request_id),
             "User-Agent": "Mozilla/5.0 (Linux; Android 11; sdk_gphone_x86 Build/RSR1.201013.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36",
-        })
-
+        }
+        response = await client.post(AUTH_URL_ORIGIN + "/login-srv/login", timeout=30, data=data, headers=headers)
         if response.status_code != 302:
             _LOGGER.error("No redirect found")
             if check_credentials:
@@ -66,8 +69,8 @@ async def auth(email, password, check_credentials=False):
                 return False
             raise MqttClientAuthenticationError("No code found")
 
-        token_response = await client.post("https://accounts.rehau.com/token-srv/token", timeout=30, data={
-            "client_id": "3f5d915d-a06f-42b9-89cc-2e5d63aa96f1",
+        token_response = await client.post(AUTH_URL_ORIGIN+"/token-srv/token", timeout=30, data={
+            "client_id": CLIENT_ID,
             "code": queries["code"],
             "grant_type": "authorization_code",
             "redirect_uri": "http://localhost:3000/#!/auth-code",
@@ -100,17 +103,18 @@ async def refresh(refresh_token):
     """Handle the refresh of the authentication token."""
 
     async with httpx.AsyncClient() as client:
-        token_response = await client.post("https://accounts.rehau.com/token-srv/token", timeout=30, data={
-            "client_id": "3f5d915d-a06f-42b9-89cc-2e5d63aa96f1",
+        token_response = await client.post(AUTH_URL_ORIGIN + "/token-srv/token", timeout=30, data={
+            "client_id": CLIENT_ID,
             "refresh_token": refresh_token,
             "grant_type": "refresh_token",
-            "client_secret": "10edca85-0623-48ad-bbbe-76b5e4ec89a9",
+            "client_secret": CLIENT_SECRET,
         }, headers={
-            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
         })
 
+
         if token_response.status_code >= 400:
-            _LOGGER.error("Could not refresh token")
             raise MqttClientAuthenticationError("Could not refresh token (status code {}) (response: {})".format(token_response.status_code, token_response.text))
 
         token_data = token_response.json()
